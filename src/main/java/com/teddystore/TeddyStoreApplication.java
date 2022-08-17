55005619e473572249efb6ec857d19e78617a6b3
AlbertViCa <albert0-v.c@hotmail.com>
2022-08-17 10:13:54 -0500

    Setup.

---
+++ src/main/java/com/teddystore/TeddyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.teddystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeddyStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeddyStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teddystore/TeddyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeddyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
