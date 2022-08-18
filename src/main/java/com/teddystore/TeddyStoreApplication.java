55821626ee8479b3d74b7595545834c72d4df4e4
AlbertCardona <albert.cardona.03@gmail.com>
2022-08-18 10:49:01 -0500

    Development profile added.

--- src/main/java/com/teddystore/TeddyStoreApplication.java
+++ src/main/java/com/teddystore/TeddyStoreApplication.java
@@ -2,8 +2,10 @@ package com.teddystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class TeddyStoreApplication {
 
     public static void main(String[] args) {
