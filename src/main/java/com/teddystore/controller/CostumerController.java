0992d250ae5432122b799c1c41c353770ffdd13b
AlbertViCa <albert0-v.c@hotmail.com>
2022-08-22 16:35:18 -0500

    Authority filter added.

--- src/main/java/com/teddystore/controller/CostumerController.java
+++ src/main/java/com/teddystore/controller/CostumerController.java
@@ -3,7 +3,9 @@ package com.teddystore.controller;
 import com.teddystore.model.Costumer;
 import com.teddystore.service.CostumerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -20,36 +22,47 @@ public class CostumerController {
     }
 
     @PostMapping(value = "register/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAuthority('SCOPE_costumer:write')")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Costumer registerUser(@RequestBody Costumer costumer) {
         return costumerService.registerCostumer(costumer);
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_costumer:read')")
+    @ResponseStatus(value = HttpStatus.FOUND)
     public Iterable<Costumer> getAllUsers() {
         return costumerService.getCostumers();
     }
 
     @GetMapping("{id}")
+    @PreAuthorize("hasAuthority('SCOPE_costumer:read')")
+    @ResponseStatus(value = HttpStatus.FOUND)
     public Optional<Costumer> getUserById(@PathVariable Long id) {
         return costumerService.getCostumerById(id);
     }
 
     @GetMapping("costumer/{username}")
+    @PreAuthorize("hasAuthority('SCOPE_costumer:read')")
+    @ResponseStatus(value = HttpStatus.FOUND)
     public Optional<Costumer> getByUsername(@PathVariable String username) {
         return costumerService.getByUsername(username);
     }
 
     @PutMapping("{id}")
+    @PreAuthorize("hasAuthority('SCOPE_costumer:write')")
     public Costumer updateUserDetails(@RequestBody Costumer costumer, @PathVariable Long id) {
         return costumerService.updateCostumerDetails(id, costumer);
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasAuthority('SCOPE_costumer:delete')")
     void deleteUserById(@PathVariable Long id) {
         costumerService.deleteCostumerById(id);
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAuthority('SCOPE_costumer:delete')")
     void deleteUsers() {
         costumerService.deleteCostumers();
     }
--- src/main/java/com/teddystore/controller/TeddyController.java
+++ src/main/java/com/teddystore/controller/TeddyController.java
@@ -3,6 +3,8 @@ package com.teddystore.controller;
 import com.teddystore.model.Teddy;
 import com.teddystore.service.TeddyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -19,36 +21,47 @@ public class TeddyController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('SCOPE_teddy:write')")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Teddy saveTeddy(@RequestBody Teddy teddy) {
         return teddyService.saveTeddy(teddy);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("id/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_teddy:read')")
+    @ResponseStatus(value = HttpStatus.FOUND)
     public Optional<Teddy> getBYId(@PathVariable Long id) {
         return teddyService.getById(id);
     }
 
     @GetMapping("{name}")
+    @PreAuthorize("hasAuthority('SCOPE_teddy:read')")
+    @ResponseStatus(value = HttpStatus.FOUND)
     public Optional<Teddy> getByName(@PathVariable String name) {
         return teddyService.getByName(name);
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_teddy:read')")
+    @ResponseStatus(value = HttpStatus.FOUND)
     public Optional<Iterable<Teddy>> getAllTeddies() {
        return teddyService.getAllTeddies();
     }
 
     @PutMapping("{id}")
+    @PreAuthorize("hasAuthority('SCOPE_teddy:write')")
     public Teddy updateTeddyDetails(@PathVariable Long id, @RequestBody Teddy teddy) {
         return teddyService.updateTeddyDetails(id, teddy);
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasAuthority('SCOPE_teddy:delete')")
     public void deleteTeddyById(@PathVariable Long id) {
         teddyService.deleteTeddyById(id);
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAuthority('SCOPE_teddy:delete')")
     public void deleteAllTeddies() {
         teddyService.deleteAll();
     }
