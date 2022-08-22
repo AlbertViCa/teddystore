7feb2abc78e700de7e82110d7275d73863dcc5c3
AlbertViCa <albert0-v.c@hotmail.com>
2022-08-22 16:34:09 -0500

    Response status not found, added.

--- src/main/java/com/teddystore/exception/CostumerNotFoundException.java
+++ src/main/java/com/teddystore/exception/CostumerNotFoundException.java
@@ -1,5 +1,9 @@
 package com.teddystore.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class CostumerNotFoundException extends RuntimeException {
 
     public CostumerNotFoundException(String message){
--- src/main/java/com/teddystore/exception/TeddyNotFoundException.java
+++ src/main/java/com/teddystore/exception/TeddyNotFoundException.java
@@ -1,5 +1,9 @@
 package com.teddystore.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class TeddyNotFoundException extends RuntimeException {
 
     public TeddyNotFoundException(String message) {
