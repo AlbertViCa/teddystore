4acbca22af0ba1d3985f948d6ba7172d66b8ab4b
AlbertCardona <albert.cardona.03@gmail.com>
2022-08-25 09:05:22 -0500

    TeddyRunner functionalities added.

--- src/main/java/com/teddystore/config/runner/TeddyRunner.java
+++ src/main/java/com/teddystore/config/runner/TeddyRunner.java
@@ -1,10 +1,15 @@
 package com.teddystore.config.runner;
 
+import com.teddystore.model.Teddy;
 import com.teddystore.service.TeddyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
+@Slf4j
 @Component
 public class TeddyRunner implements CommandLineRunner {
 
@@ -18,5 +23,18 @@ public class TeddyRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        log.info("---------- CREATING TEDDY ----------");
+
+        Teddy teddy = Teddy.builder()
+                .name("Bard")
+                .details("Snow Fest Bard")
+                .size(15.00)
+                .price(BigDecimal.valueOf(300.00))
+                .imageURL("image.com")
+                .build();
+
+        log.info("---------- SAVING TEDDY ----------");
+
+        teddyService.saveTeddy(teddy);
     }
 }
