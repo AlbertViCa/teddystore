ee7b8a9e9f1135f23cc2c3973eefaf97378f3548
AlbertViCa <albert0-v.c@hotmail.com>
2022-09-02 15:29:28 -0500

    Mapping added.

--- src/main/java/com/teddystore/controller/EmployeeController.java
+++ src/main/java/com/teddystore/controller/EmployeeController.java
@@ -1,4 +1,9 @@
 package com.teddystore.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/employees/")
 public class EmployeeController {
 }
