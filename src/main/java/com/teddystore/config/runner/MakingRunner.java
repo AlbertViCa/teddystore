92a63846f31c33895dc0ae8973166ebb0603a425
AlbertViCa <albert0-v.c@hotmail.com>
2022-09-13 21:31:26 -0500

    MakingRunner implements CommandLineRunner.

--- src/main/java/com/teddystore/config/runner/MakingRunner.java
+++ src/main/java/com/teddystore/config/runner/MakingRunner.java
@@ -1,9 +1,14 @@
 package com.teddystore.config.runner;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(8)
-public class MakingRunner {
+public class MakingRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
